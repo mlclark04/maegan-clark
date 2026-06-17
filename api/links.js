35d92fd6072ce62123ea60ee0acb13ba3bfbcfc2
mlclark04 <maeganlclark@gmail.com>
2026-06-17 10:00:26 -0500

    Update links.js

--- api/links.js
+++ api/links.js
@@ -1,7 +1,25 @@
+import https from 'https';
+
+function fetchWithRedirect(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
+        return fetchWithRedirect(res.headers.location).then(resolve).catch(reject);
+      }
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
+      res.on('error', reject);
+    }).on('error', reject);
+  });
+}
+
 export default async function handler(req, res) {
-  const url = 'https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv';
-  const response = await fetch(url);
-  const csv = await response.text();
-  res.setHeader('Cache-Control', 's-maxage=60');
-  res.status(200).send(csv);
+  try {
+    const csv = await fetchWithRedirect('https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv');
+    res.setHeader('Cache-Control', 's-maxage=60');
+    res.status(200).send(csv);
+  } catch (e) {
+    res.status(500).send('Error: ' + e.message);
+  }
 }
