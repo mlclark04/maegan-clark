a7c898eb119d57cd5b8d0f83757a3b9baa634fb4
mlclark04 <maeganlclark@gmail.com>
2026-06-17 10:24:55 -0500

    Update links.js

--- api/links.js
+++ api/links.js
@@ -1,25 +1,13 @@
-const https = require('https');
+export const config = { runtime: 'edge' };
 
-function fetchWithRedirect(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      if (res.statusCode >= 300 && res.statusCode < 400 && res.headers.location) {
-        return fetchWithRedirect(res.headers.location).then(resolve).catch(reject);
-      }
-      let data = '';
-      res.on('data', chunk => data += chunk);
-      res.on('end', () => resolve(data));
-      res.on('error', reject);
-    }).on('error', reject);
+export default async function handler(req) {
+  const url = 'https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv';
+  const response = await fetch(url);
+  const csv = await response.text();
+  return new Response(csv, {
+    headers: {
+      'Content-Type': 'text/plain',
+      'Cache-Control': 's-maxage=60',
+    },
   });
-}
-
-module.exports = async function handler(req, res) {
-  try {
-    const csv = await fetchWithRedirect('https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv');
-    res.setHeader('Cache-Control', 's-maxage=60');
-    res.status(200).send(csv);
-  } catch (e) {
-    res.status(500).send('Error: ' + e.message);
-  }
 }
