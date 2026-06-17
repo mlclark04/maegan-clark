374e8f986c12524762826a98b68ed6167691815c
mlclark04 <maeganlclark@gmail.com>
2026-06-17 13:02:41 -0500

    Update links.js

--- api/links.js
+++ api/links.js
@@ -1,7 +1,7 @@
 export const config = { runtime: 'edge' };
 
 export default async function handler(req) {
-  const url = 'https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv';
+  const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQHvYHhjWVdIugqzZ4xyG5bOUadzz2m1A9Ca4PK0KRBxoDNQT8w6n7eg2jkS9Nu0u7g9SwZRNTUa74z/pub?output=csv';
   const response = await fetch(url);
   const csv = await response.text();
   return new Response(csv, {
