11f0cff5385605bb5a7dedef819b1a1028f76be0
mlclark04 <maeganlclark@gmail.com>
2026-06-17 09:20:57 -0500

    Delete links.js

--- links.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const url = 'https://docs.google.com/spreadsheets/d/1iKeamsUBU8DIXbHgrNJQzurIqLSDbKV3c3s_mBXGysw/pub?output=csv';
-  const response = await fetch(url);
-  const csv = await response.text();
-  res.setHeader('Cache-Control', 's-maxage=60');
-  res.status(200).send(csv);
-}
